7f78fa94e501ae7c852d84ceae1a277a6d66023d
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2021-10-26 22:16:05 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -3,7 +3,7 @@
 
 //typedef void (*alerter_funcptr)();
 typedef int (*alerter_funcptr)(int,int);
-int check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+int check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
 {
     int a = maxThreshold;
     int b = computedStats.average;
