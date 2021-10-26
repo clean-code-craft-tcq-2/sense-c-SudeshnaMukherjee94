3afab9ff32821c305f97c55bf17fe0b9448c8839
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2021-10-26 17:29:50 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,10 +1,26 @@
 #include "stats.h"
-struct Stats s;
+
 struct Stats compute_statistics(const float* numberset, int setlength) {
+ struct Stats s;
+   int sum=0,i;
+   s.min=numberset[0];
+   s.max=numberset[0];
+   for(i=0;i<setlength;i++)
+   {
+      sum+=numset[i];
+      if (s.min>=numberset[i])
+      {
+          s.min=numberset[i];
+      }
+      if (s.max<=numberset[i])
+      {
+          s.max=numberset[i];
+      }
+   }
    
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    s.average = sum/setlength;
+//    s.min = 0;
+//    s.max = 0;
 }
 
 int emailAlertCallCount = 0;
