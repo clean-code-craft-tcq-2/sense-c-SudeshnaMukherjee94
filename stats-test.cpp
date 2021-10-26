b0931a515af856d25d4490c7a23c2ad787801205
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2021-10-26 18:14:14 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -5,13 +5,7 @@
 //#include "stats.c"
 #include <stdlib.h>
 #include <math.h>
-#define computeStats {"",0,0.0f} 
-struct Stats //new
-{
-    float average;
-    float max;
-    float min;
-};
+
 
 TEST_CASE("reports average, minimum and maximum") {
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
