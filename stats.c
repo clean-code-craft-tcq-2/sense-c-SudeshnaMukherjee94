7b3fa311ed751211c002e62288cecd879f95c0e4
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2021-10-26 22:50:57 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,11 +1,11 @@
 #include "stats.h"
-/*#define computeStats {"",0,0.0f} 
+#define computeStats {"",0,0.0f} 
 struct Stats //new
 {
     float average;
     float max;
     float min;
-};*/
+};
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
  struct Stats s; 
@@ -31,6 +31,18 @@ int ledAlerter (int max, int threshold)
         return 1;
     else 
         return 0;
+}
+
+int check_and_alert (float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    int a = maxThreshold;
+    int b = computedStats.average;
+    int Alert1 = alerters[0](a,b);
+    int Alert2 = alerters[1](a,b);
+    if (Alert1+Alert2 > 0)
+    return 1;
+    else
+    return 0;  
 }
 
 int emailAlertCallCount = 0;
