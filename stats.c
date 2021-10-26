9c3cafc5d739d506bceab1f1c272defb8b138fe1
SudeshnaMukherjee94 <91111960+SudeshnaMukherjee94@users.noreply.github.com>
2021-10-26 22:18:54 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -33,5 +33,17 @@ int ledAlerter (int max, int threshold)
         return 0;
 }
 
+int check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    int a = maxThreshold;
+    int b = computedStats.average;
+    int Alert1 = alerters[0](a,b);
+    int Alert2 = alerters[1](a,b);
+    if (Alert1+Alert2 > 0)
+    return 1;
+    else
+    return 0;  
+}
+
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
